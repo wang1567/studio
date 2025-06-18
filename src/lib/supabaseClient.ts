12df8411f58f26c63b5adf2d49976a6e860feb51
王國全 <0966178691wang@gmail.com>
2025-06-18 05:02:05 +0000

    URL:https://hkjclbdisriyqsvcpmnp.supabase.co

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,11 +5,11 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL. Please check your .env.local file.");
 }
 
 if (!supabaseAnonKey) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY. Please check your .env.local file.");
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
