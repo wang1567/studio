981f94a07c3d7621167ccc743bc3c05f5b25041e
王國全 <0966178691wang@gmail.com>
2025-06-20 09:21:56 +0000

    ReferenceError: React is not defined     at AuthForm (https://6000-fireb

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from 'react'; // Added this line
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
